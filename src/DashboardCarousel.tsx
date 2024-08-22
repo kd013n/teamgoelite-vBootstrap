6516c5881fcc23ba319f26770089a647715eeb69
teamgoelite <axateamgoelite7@gmail.com>
2024-08-22 12:14:26 +0800

    minor dashboard/navbar fixes v1.1

--- src/DashboardCarousel.tsx
+++ src/DashboardCarousel.tsx
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import './assets/css/DashboardCarousel.css'
+import dbbanner1 from './assets/images/db-banner1.png'
+import dbbanner2 from './assets/images/db-banner2.png'
 
 function DashboardCarousel() {
     return <>
@@ -8,15 +10,19 @@ function DashboardCarousel() {
         <div className="carousel-inner h-75">
             <div className="carousel-item h-75 active">
                 <div className="dashboard-banner1 position-absolute">
-                    <h1 className="display-1">Display 1</h1>
+                    <h1 className="display-1">TEAM GO</h1> <br />
+                    <h1 className="display-1">ELITE</h1>
                 </div>
-                <img src="https://images.pexels.com/photos/26493650/pexels-photo-26493650/free-photo-of-ducks-by-the-stream-in-sunlight.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="object-fit-cover w-100" alt="..."/ >
+                <img src={dbbanner1} className="object-fit-cover w-100" alt="..."/ >
             </div>
             <div className="carousel-item h-75">
-                <img src="https://images.pexels.com/photos/127027/pexels-photo-127027.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="object-fit-cover w-100" alt="..."/ >
+                <img src={dbbanner2} className="object-fit-cover w-100" alt="..."/ >
             </div>
             <div className="carousel-item h-75">
-                <img src="https://images.pexels.com/photos/288621/pexels-photo-288621.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="object-fit-cover w-100" alt="..."/ >
+                <img src={dbbanner1} className="object-fit-cover w-100" alt="..."/ >
+            </div>
+            <div className="carousel-item h-75">
+                <img src={dbbanner2} className="object-fit-cover w-100" alt="..."/ >
             </div>
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
