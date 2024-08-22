e04183673054bcc2392b687f19c1bb79fbc4747a
teamgoelite <axateamgoelite7@gmail.com>
2024-08-22 20:49:45 +0800

    minor fixes v1.3

--- src/DashboardCarousel.tsx
+++ src/DashboardCarousel.tsx
@@ -1,54 +1,101 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
-import './assets/css/DashboardCarousel.css'
-import dbbanner1 from './assets/images/db-banner1.png'
-import dbbanner2 from './assets/images/db-banner2.png'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "./assets/css/DashboardCarousel.css";
+import dbbanner1 from "./assets/images/db-banner1.png";
+import dbbanner2 from "./assets/images/db-banner2.png";
 
 function DashboardCarousel() {
-    return <>
-    <div id="carouselExampleAutoplaying" className="carousel slide " data-bs-ride="carousel">
+  return (
+    <>
+      <div
+        id="carouselExampleAutoplaying"
+        className="carousel slide "
+        data-bs-ride="carousel"
+      >
         <div className="carousel-inner">
-            <div className="carousel-item active">
-                <div className="dashboard-banner1 position-absolute">
-                    <p className="display-1">TEAM <span className="spc1 thicc">GO</span></p> 
-                    <p className="display-1">ELITE</p>
-                </div>
-                <img src={dbbanner1} className="object-fit-cover w-100" alt="..."/ >
+          <div className="carousel-item active" data-bs-interval="2300">
+            <div className="dashboard-banner position-absolute">
+              <div className="dbline">
+                <p className="disp1">TEAM </p>
+                <p className="disp2"> GO</p>
+              </div>
+              <div className="dbline">
+                <p className="disp1">ELITE</p>
+              </div>
             </div>
-            <div className="carousel-item">
-                <div className="dashboard-banner1 position-absolute">
-                    <p className="display-1">WHAT'S </p>
-                    <p className="display-1"><span className="spc1 thicc">NEW?</span></p>
-                </div>
-                <img src={dbbanner2} className="object-fit-cover w-100" alt="..."/ >
+            <div className="dashboard-text">
+              <div className="d-grid gap-2 col-6 mx-auto">
+                <button className="btn btn-primary" type="button">
+                  Button
+                </button>
+                <button className="btn btn-primary" type="button">
+                  Button
+                </button>
+              </div>
             </div>
-            <div className="carousel-item">
-                <div className="dashboard-banner1 position-absolute">
-                    <p className="display-1">THE TGE</p>
-                    <p className="display-1"><span className="spc1 thicc">MISSION</span></p>
-                </div>
-                <img src={dbbanner1} className="object-fit-cover w-100" alt="..."/ >
+            <img src={dbbanner1} className="object-fit-cover w-100" alt="..." />
+          </div>
+          <div className="carousel-item" data-bs-interval="2300">
+            <div className="dashboard-banner position-absolute">
+              <div className="dbline">
+                <p className="disp1">WHAT'S </p>
+              </div>
+              <div className="dbline">
+                <p className="disp2">NEW?</p>
+              </div>
             </div>
-            <div className="carousel-item">
-                <div className="dashboard-banner1 position-absolute">
-                    <p className="display-1">THE TGE</p>
-                    <p className="display-1"><span className="spc1 thicc">VISION</span></p>
-                </div>
-                <img src={dbbanner2} className="object-fit-cover w-100" alt="..."/ >
+            <img src={dbbanner2} className="object-fit-cover w-100" alt="..." />
+          </div>
+          <div className="carousel-item" data-bs-interval="2300">
+            <div className="dashboard-banner position-absolute">
+              <div className="dbline">
+                <p className="disp1">THE TGE</p>
+              </div>
+              <div className="dbline">
+                <p className="disp2">MISSION</p>
+              </div>
             </div>
+            <img src={dbbanner1} className="object-fit-cover w-100" alt="..." />
+          </div>
+          <div className="carousel-item" data-bs-interval="2300">
+            <div className="dashboard-banner position-absolute">
+              <div className="dbline">
+                <p className="disp1">THE TGE</p>
+              </div>
+              <div className="dbline">
+                <p className="disp2">VISION</p>
+              </div>
+            </div>
+            <img src={dbbanner2} className="object-fit-cover w-100" alt="..." />
+          </div>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
         </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
         </button>
-    </div>
-        
-
+      </div>
     </>
+  );
 }
 
-export default DashboardCarousel
+export default DashboardCarousel;
