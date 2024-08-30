7d8e8d408bb9ef23719e5b67de89b164fc66e2bf
kd013n <leipzigsurrealist@gmail.com>
2024-08-30 16:01:35 +0800

    v1.5.1 added a title to the Dashboard Cards

--- src/DashboardCards.tsx
+++ src/DashboardCards.tsx
@@ -7,131 +7,134 @@ import imagerand from "./assets/images/db-banner1.png";
 function DashboardCards() {
   return (
     <>
-      <div id="carouselExample" className="carousel slide">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <div className="row card-cluster">
-              <div className="col-sm">
-                <div className="card">
-                  <img src={imagerand} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" className="btn card-btn">
-                      Go somewhere
-                    </a>
+      <div className="container-fluid dashboardcards-container">
+        <p className="display-1 dashboardcards-title">Explore Team Go Elite</p>
+        <div id="carouselExample" className="carousel slide cards-carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <div className="row card-cluster">
+                <div className="col-sm">
+                  <div className="card">
+                    <img src={imagerand} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a href="#" className="btn card-btn">
+                        Go somewhere
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm">
+                  <div className="card">
+                    <img src={imagerand} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a href="#" className="btn card-btn">
+                        Go somewhere
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm">
+                  <div className="card">
+                    <img src={imagerand} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a href="#" className="btn card-btn">
+                        Go somewhere
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="col-sm">
-                <div className="card">
-                  <img src={imagerand} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" className="btn card-btn">
-                      Go somewhere
-                    </a>
+            </div>
+            <div className="carousel-item">
+              <div className="row card-cluster">
+                <div className="col-sm">
+                  <div className="card">
+                    <img src={imagerand} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a href="#" className="btn card-btn">
+                        Go somewhere
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-sm">
-                <div className="card">
-                  <img src={imagerand} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" className="btn card-btn">
-                      Go somewhere
-                    </a>
+                <div className="col-sm">
+                  <div className="card">
+                    <img src={imagerand} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a href="#" className="btn card-btn">
+                        Go somewhere
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm">
+                  <div className="card">
+                    <img src={imagerand} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a href="#" className="btn card-btn">
+                        Go somewhere
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="carousel-item">
-            <div className="row card-cluster">
-              <div className="col-sm">
-                <div className="card">
-                  <img src={imagerand} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" className="btn card-btn">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm">
-                <div className="card">
-                  <img src={imagerand} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" className="btn card-btn">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm">
-                <div className="card">
-                  <img src={imagerand} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" className="btn card-btn">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
     </>
   );
--- src/FooterMobile.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
-import "./assets/css/FooterMobile.css";
-
-function FooterMobile() {
-  return <></>;
-}
